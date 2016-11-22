dd804d64bef2f6a3d8f24f9838a6edae63fecf79
Raymond Masson <rmasson@qxlricardo.internal>
2016-11-22 12:05:35 +0100

    constructor

--- structs/structs.go
+++ structs/structs.go
@@ -5,12 +5,6 @@ import (
 )
 
 func main() {
-	type courseMeta struct {
-		Author string
-		Level  string
-		Rating float64
-	}
-
 	//var DockerDeepDive courseMeta
 	//DockerDeepDive := new(courseMeta)
 
@@ -26,4 +20,36 @@ func main() {
 	DockerDeepDive.Rating = 1
 
 	fmt.Println("\nDocker Deep Dive Rating is:", DockerDeepDive.Rating)
+
+	myCourse := newCourseMeta()
+
+	fmt.Println(myCourse)
+
+	myCourse2 := newCourseMeta2("rmasson", "pro", 2)
+
+	fmt.Println(myCourse2)
+}
+
+type courseMeta struct {
+	Author string
+	Level  string
+	Rating float64
+}
+
+func newCourseMeta() *courseMeta {
+	courseMeta := courseMeta{}
+	courseMeta.Author = "Ray"
+	courseMeta.Level = "Beginner"
+	courseMeta.Rating = 5
+
+	return &courseMeta
+}
+
+func newCourseMeta2(author, level string, rating float64) *courseMeta {
+	courseMeta := courseMeta{}
+	courseMeta.Author = author
+	courseMeta.Level = level
+	courseMeta.Rating = rating
+
+	return &courseMeta
 }
